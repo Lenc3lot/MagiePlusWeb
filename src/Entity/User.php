3b0921277edf094988d77bf356a69f742a6700d3
Maxime Vernoux <mxm.vernoux@gmail.com>
2025-02-14 22:46:07 +0100

    fix User.php : Ajout de la l'entity User

---
+++ src/Entity/User.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity;
+
+class User
+{
+    private ?int $id = null;
+    private ?string $nom = null;
+    private ?string $prenom = null;
+    private ?string $login = null;
+    private ?string $password = null;
+    public function __construct($params = null) {
+        if (!is_null($params)) {
+            foreach ($params as $cle => $valeur) {
+                $this->$cle = $valeur;
+            }
+        }
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+        return $this;
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): self
+    {
+        $this->login = $login;
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
+}
