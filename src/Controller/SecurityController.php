16d7e2c27596802f8cea655e1baa2c749018a5de
Maxime Vernoux <mxm.vernoux@gmail.com>
2025-02-20 23:14:25 +0100

    feat LoginController : Ajout du back de la fonction de login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use JetBrains\PhpStorm\NoReturn;
 use ReflectionClass;
 use Tools\MyTwig;
+use Tools\Repository;
 
 class SecurityController
 {
@@ -12,9 +14,32 @@ class SecurityController
      */
     public function login(): void
     {
+        if (isset($_SESSION['user'])) {
+            header('Location: /index.php?c=home&a=index');
+            exit();
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $login = $_POST['login'];
+            $password = $_POST['password'];
+
+            // Assuming you have a UserRepository to fetch user data
+            $userRepository = Repository::getRepository("App\Entity\User");
+            $user = $userRepository->findOneBy(['login' => $login]);
+
+            if ($user && $password === $user->getPassword()) {
+                $_SESSION['user'] = $user->getId();
+                $_SESSION['login'] = $user->getLogin();
+                header('Location: /index.php?c=home&a=index');
+                exit();
+            } else {
+                $params['error'] = 'Invalid credentials';
+            }
+        }
+
         $r = new ReflectionClass($this);
         $vue = str_replace('Controller', 'view', $r->getshortName()) . "/login.html.twig";
-        MyTwig::afficheVue($vue, ["title" => "Login"]);
+        MyTwig::afficheVue($vue, $params ?? ["title" => "Login"]);
     }
 
     public function register(): void
