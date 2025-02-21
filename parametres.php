b3f7f07586f4c9fff3d312cb2acd28c18659ca4e
Maxime Vernoux <mxm.vernoux@gmail.com>
2025-02-21 09:18:53 +0100

    fix connexionBDD : Modification des informartions de connexion

--- parametres.php
+++ parametres.php
@@ -1,8 +1,7 @@
 <?php
 
-define('DATABASE_URL', "localhost");
-define('DATABASE_USER', "root");
-define('DATABASE_PWD', "");
-define('DATABASE_NAME', "noto_magieplus");
-define('CNSTRING', "mysql:host=" . DATABASE_URL . ";dbname=" . DATABASE_NAME . "; charset=UTF8");
-
+ const DATABASE_URL = "localhost";
+const DATABASE_USER = "db_user";
+const DATABASE_PWD = "db_password";
+const DATABASE_NAME = "noto";
+const CNSTRING = "mysql:host=" . DATABASE_URL . ";dbname=" . DATABASE_NAME . "; charset=UTF8";
